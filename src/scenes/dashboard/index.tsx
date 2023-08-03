115e149032d0c4a0dd57f58bc22acede8ac4aae2
Balogun Kayode <kayodefredrickbalogun@gmail.com>
2023-08-04 00:18:44 +0100

    modules

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Header from '../../components/Header'
+import { Box } from '@mui/material'
 
 const Dashboard = () => {
-  return (
-    <div>index</div>
-  )
+    return (
+        <Box m='20px'>
+            <Box display='flex' justifyContent='space-between' alignItems='center'>
+                <Header title='DASHBOARD' subtitle='Welcome to your dashboard' />
+            </Box>
+        </Box>
+    )
 }
 
-export default Dashboard
+export default Dashboard;
